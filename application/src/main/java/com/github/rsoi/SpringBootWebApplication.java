ef578f9aa0862dbce2abd822a4c59beb39904d31
straightth <speciallyfororigin@gmail.com>
2023-02-07 00:49:55 +0300

    Fixing CLI start

--- application/src/main/java/com/github/rsoi/SpringBootWebApplication.java
+++ application/src/main/java/com/github/rsoi/SpringBootWebApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootConsoleApplication {
+public class SpringBootWebApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootConsoleApplication.class, args);
+        SpringApplication.run(SpringBootWebApplication.class, args);
 	}
 }
--- application/src/test/java/SpringBootWebApplicationTest.java
+++ application/src/test/java/SpringBootWebApplicationTest.java
@@ -1,7 +1,7 @@
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class SpringBootConsoleApplicationTest {
+public class SpringBootWebApplicationTest {
     @Test
     public void example() {
         Assertions.assertThat(true).isTrue();
